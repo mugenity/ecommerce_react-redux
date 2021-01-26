58f8370aa543c8b9f75ba22fc5774e70d4b287f9
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-26 13:51:51 +0100

    custom buttom component and installation of firebase

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import "./signin.styles.scss";
 
@@ -47,7 +48,7 @@ class SignIn extends React.Component {
             required
           />
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     );
