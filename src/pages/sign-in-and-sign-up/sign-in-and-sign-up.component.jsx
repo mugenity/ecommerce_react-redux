59def3f351ce25ba8e23fc5b4b64b007d4e2c9a9
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-28 13:43:41 +0100

    SignUp with Name and Password

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SignUp from "../../components/sign-up/sign-up.components";
 import SignIn from "../../components/signin/signin.component";
 
 import "./sign-in-and-sign-up.styles.scss";
@@ -6,6 +7,7 @@ import "./sign-in-and-sign-up.styles.scss";
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
